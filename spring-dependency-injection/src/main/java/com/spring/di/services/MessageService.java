01778517fc0109e006c95144a28b9806214b2faf
anurag <test@test.com>
2015-01-18 23:46:39 +0530

    Commit for spring dependecny Injections

---
+++ spring-dependency-injection/src/main/java/com/spring/di/services/MessageService.java
@@ -0,0 +1,6 @@
+package com.spring.di.services;
+
+public interface MessageService {
+
+	boolean sendMessage(String msg, String rec);
+}
---
+++ spring-dependency-injection/src/main/java/com/spring/di/test/ClientApplication.java
@@ -0,0 +1,20 @@
+package com.spring.di.test;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import com.spring.di.configuration.DIConfiguration;
+import com.spring.di.consumer.MyApplication;
+
+public class ClientApplication {
+
+	public static void main(String[] args) {
+		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(DIConfiguration.class);
+		MyApplication app = context.getBean(MyApplication.class);
+		
+		app.processMessage("Hi Pankaj", "pankaj@abc.com");
+		
+		//close the context
+		context.close();
+	}
+
+}
